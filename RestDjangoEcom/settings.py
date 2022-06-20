5cd50321a9dc93244b9a03574c589085a8afd85f
QuocTuanIT <kutuanonline199@gmail.com>
2022-06-20 07:02:36 +0700

    first commit

---
+++ RestDjangoEcom/settings.py
@@ -0,0 +1,134 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-n#@y*%v5g1p53=fi-2_g)yw#ti+%5j5(m#=_ua$9-rk*m78#5j'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['https://tqt-be-vue.herokuapp.com']
+
+STRIPE_SECRET_KEY = 'sk_test_51L3IEXGA7goTPkXeUfAWFoIjQ278h8eDRv3GFc2NNfTk86X2VQqE8COzMdvDXptAL1B17SzUZdGZQneRach4ELC300x7kIyrjh'
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'djoser',
+
+    'product',
+    'order',
+    'blog',
+
+]
+
+CORS_ALLOWED_ORIGINS = ['https://tqt-be-vue.herokuapp.com', 'https://tqt-vue-shop.netlify.app'
+    'http://localhost:8081', 'http://localhost:8080', 'http://localhost:8082',
+]
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'RestDjangoEcom.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'RestDjangoEcom.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ blog/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
+from django.utils.text import slugify
+from autoslug import AutoSlugField
+from product.utils import make_thumbnail
+from tinymce.models import HTMLField
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    body = HTMLField()
+    slug = AutoSlugField(populate_from='title', unique_with='title')
+
+    image = models.ImageField(upload_to='uploads/blogs/', blank=True)
+    thumbnail = models.ImageField(upload_to='uploads/blogs/thumbnails/', blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}/"
+
+    def __str__(self):
+        return self.title
+
+    def get_image(self):
+        if self.image:
+            return f'{self.image.url}'
+        return ''
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return f'{self.thumbnail.url}'
+        if self.image:
+            size = (400, 300)
+            self.thumbnail = make_thumbnail(self.image, size)
+            self.save()
+            return f'{self.thumbnail.url}'
+        else:
+            return ''
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_description(self):
+        if len(self.description) > 20:
+            return f'{self.description[:20]}...'
+        else:
+            return self.description
+
+    def get_title(self):
+        if len(self.title) > 40:
+            return f'{self.title[:40]}...'
+        else:
+            return self.title
